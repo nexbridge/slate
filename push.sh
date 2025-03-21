0fcc89d6a0347b9a9ff4ebbf01e6757e7811647d
nexbridge <13848887+nexbridge@users.noreply.github.com>
2025-03-21 14:29:03 +0000

    Tweaked push.sh

--- push.sh
+++ push.sh
@@ -2,9 +2,12 @@
 
 THIS_DIR=`dirname $0`
 
+cd $THIS_DIR
+
 rm -rf $THIS_DIR/build/*
 
 bundle exec middleman build --clean
 
 scp -r $THIS_DIR/build/* root@web1.do.nexbridge.co.uk:/var/www/apidocs.nexbridge.co.uk
 
+cd -
